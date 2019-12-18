d803a3e6563af058b8675085f1041c82a991a54a
Daeun Kim <9190k@naver.com>
2019-12-18 11:10:42 +0900

    Add dags

---
+++ dags/kube_pod.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+
+default_args = {
+    'owner': 'neos',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 11, 1),
+    'email': ['kimdaeun@neosapience.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'kube_sample', default_args=default_args, schedule_interval=timedelta(minutes=3))
+
+
+start = DummyOperator(task_id='t1', dag=dag)
+
+passing = KubernetesPodOperator(namespace='airflow-dev',
+                          image="python:3.6-slim",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"app": "airflow"},
+                          node_selectors={"role": "data-pipeline"},
+                          service_account_name="sa-airflow",
+                          name="passing-test",
+                          task_id="passing-task",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+failing = KubernetesPodOperator(namespace='airflow-dev',
+                          image="ubuntu:1604",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"app": "airflow"},
+                          node_selectors={"role": "data-pipeline"},
+                          service_account_name="sa-airflow",
+                          name="fail",
+                          task_id="failing-task",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+passing.set_upstream(start)
+failing.set_upstream(start)
